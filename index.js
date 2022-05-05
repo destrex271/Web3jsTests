0dabb2877e5bd557d1764ad5f698160f785418bd
Akshat Jaimini <destrex271@gmail.com>
2022-05-06 01:07:06 +0530

    Data

--- index.js
+++ index.js
@@ -6,7 +6,26 @@ const web3 = new Web3('http://127.0.0.1:7545')
 const acc1 = '0x8C7247c80C6185B5c4546Bea69d13A31Eb8e7D2a'
 const acc2 = '0xa72DFD4915281D366E23d008400989FA2C8C4787'
 
-const pk1 = Buffer.from(process.env.PK1)
-const pk2 = Buffer.from(process.env.PK2)
+const pk1 = Buffer.from(process.env.PK1, 'hex')
+const pk2 = Buffer.from(process.env.PK2, 'hex')
 
-// Building transaction
+web3.eth.getTransactionCount(acc1, (err, txCount)=>{
+    // Building transaction
+    const txObj = {
+        nonce: web3.utils.toHex(txCount),
+        to: acc2,
+        val: web3.utils.toHex(web3.utils.toWei('1','ether')),
+        gasLimit: web3.utils.toHex(2100),
+        gasPrice: web3.utils.toHex(web3.utils.toWei('10','gwei'))
+    }
+    // Sign it
+    const tx = new Tx.Transaction(txObj)
+    tx.sign(pk1)
+    const serializedTransaction = tx.serialize()
+    const raw = '0x' + serializedTransaction.toString()
+    // Broadcast it
+    web3.eth.sendSignedTransaction(raw, (err, txhash)=>{
+        console.log('txHash:', txhash)
+    })
+})
+
